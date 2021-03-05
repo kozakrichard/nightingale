b650395862d5267a28c31360a04ece58146fc1f1
joshnatis <joshnatis@gmail.com>
2021-03-05 00:39:21 -0500

    Upload new utilities
    Complete scrape_freemidi.py (albeit terribly), create utility for removing non-MIDI files from dataset

--- utilities/scrape_freemidi.py
+++ utilities/scrape_freemidi.py
@@ -1,21 +1,29 @@
 #https://freemidi.org/genre
-"""
-todo
-- handle pagination (artist page has multiple pages)
-	+ https://freemidi.org/artist-284-blink-182-P-3
-	+ just add the -P-1, -P-2, -P-3 until it sends a 404
-- figure out how to get actual file from download page
-	+ ajax post request nonsense
-	+ selenium simulate click?
-"""
 
 import requests
 import urllib
 from bs4 import BeautifulSoup
 import sys
+import time
+from selenium import webdriver
+import selenium
 
 USER_AGENT = 'curl/7.54.0' # why not lmfao
 HEADERS = {'user-agent': USER_AGENT}
+
+WEBDRIVER=""
+"""
+WEBDRIVER_OPTIONS = webdriver.ChromeOptions()
+PATH_TO_ADBLOCK = "/home/josh/.config/chromium/Default/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm/1.33.2_2"
+WEBDRIVER_OPTIONS.add_argument("-load-extension=" + PATH_TO_ADBLOCK)
+WEBDRIVER_OPTIONS.add_argument("-page_load_strategy=eager")
+WEBDRIVER_OPTIONS.add_argument("--headless")
+WEBDRIVER_OPTIONS.add_experimental_option("prefs",
+	{"download.default_directory" : "/home/josh/nightingale/data/freemidi.org/"})
+WEBDRIVER = webdriver.Chrome(options=WEBDRIVER_OPTIONS)
+"""
+
+
 
 CATEGORIES = [
 	"rock",
@@ -46,20 +54,36 @@ def error(*msg, displayGenres=False):
 		print("Available genres:\n *", "\n * ".join(CATEGORIES))
 	exit(1)
 
+def download(url, fn):
+	global WEBDRIVER
+	WEBDRIVER_OPTIONS = webdriver.ChromeOptions()
+	PATH_TO_ADBLOCK = "/home/josh/.config/chromium/Default/Extensions/cjpalhdlnbpafiamejdnhcphjbkeiagm/1.33.2_2"
+	WEBDRIVER_OPTIONS.add_argument("-load-extension=" + PATH_TO_ADBLOCK)
+	WEBDRIVER_OPTIONS.add_argument("-page_load_strategy=eager")
+	WEBDRIVER_OPTIONS.add_argument("--headless")
+	WEBDRIVER_OPTIONS.add_experimental_option("prefs",
+		{"download.default_directory" : "/home/josh/nightingale/data/freemidi.org/"})
+
+	WEBDRIVER = webdriver.Chrome(options=WEBDRIVER_OPTIONS)
+	WEBDRIVER.get(url)
+	WEBDRIVER.execute_script('document.getElementById("downloadmidi").click();')
+	time.sleep(1) #all good things take time...
+	WEBDRIVER.quit()
+
 def scrapeSong(song_page):
 	url = "https://freemidi.org/" + song_page
-	response = requests.get(url, headers=HEADERS)
-	if response.status_code != 200:
+	try:
+		response = requests.get(url, headers=HEADERS)
+		if response.status_code != 200:
+			return
+	except requests.exceptions.RequestException as e:
+		print(e)
 		return
 
-	html = response.text
-	soup = BeautifulSoup(html, 'html.parser')
-	href = soup.find("a", {"id": "downloadmidi"}).get("href")
-	url = "https://freemidi.org/" + href
-
 	fn = str(n_artist) + "-" + str(n_song) + ".midi"
-	try: urllib.request.urlretrieve(url, fn)
-	except urllib.error.HTTPError:
+	try: download(url, fn)
+	except selenium.common.exceptions.WebDriverException as e:
+		print(e)
 		return
 	print("* Downloading", fn, end="\r")
 
@@ -68,6 +92,31 @@ def scrapeArtist(artist_page):
 	global n_artist
 
 	url = "https://freemidi.org/" + artist_page
+
+	page = 0
+	page_exists = True
+	while page_exists:
+		page_url = url + "-P-" + str(page)
+		try:
+			response = requests.get(page_url, headers=HEADERS)
+		except requests.exceptions.RequestException as e:
+			print(e)
+			break
+
+		html = response.text
+		soup = BeautifulSoup(html, 'html.parser')
+
+		page_exists = (response.status_code == 200) and (html.find("Error no title") == -1)
+		if not page_exists: break
+
+		#first two results are not songs
+		for song in soup.find_all("a", itemprop="url")[2:]:
+			scrapeSong(song.get("href"))
+			n_song += 1
+
+		page += 1
+
+	"""
 	response = requests.get(url, headers=HEADERS)
 	if response.status_code != 200:
 		return
@@ -78,6 +127,7 @@ def scrapeArtist(artist_page):
 	for song in soup.find_all("a", itemprop="url")[2:]:
 		scrapeSong(song.get("href"))
 		n_song += 1
+	"""
 
 	n_artist += 1
 	n_song = 1
@@ -86,7 +136,7 @@ def scrapeArtist(artist_page):
 def scrape(genre):
 	genre = urllib.parse.quote(genre)
 
-	url = "https://freemidi.org/genre-" + genre 
+	url = "https://freemidi.org/genre-" + genre
 	response = requests.get(url, headers=HEADERS)
 	if response.status_code != 200:
 		return
@@ -105,7 +155,9 @@ def main():
 			error("No such genre.", displayGenres=True)
 		else:
 			scrape(sys.argv[1])
+			WEBDRIVER.quit()
 try:
 	main()
 except KeyboardInterrupt:
+	WEBDRIVER.quit()
 	exit(0)
